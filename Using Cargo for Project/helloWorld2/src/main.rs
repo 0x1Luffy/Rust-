f6dec60ced51e2f219dede5f7926c625f598a59c
0x1Luffy <chetankesare06@gmail.com>
2024-07-26 12:44:01 +0530

    Finished Introductory Part

---
+++ Using Cargo for Project/helloWorld2/src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Hello, world! , this is done using Cargo package manager");
+}
---
+++ Using Cargo for Project/helloWorld2/target/debug/deps/helloWorld2.d
@@ -0,0 +1,5 @@
+E:\Rust\Using Cargo for Project\helloWorld2\target\debug\deps\helloWorld2.exe: src/main.rs
+
+E:\Rust\Using Cargo for Project\helloWorld2\target\debug\deps\helloWorld2.d: src/main.rs
+
+src/main.rs:
