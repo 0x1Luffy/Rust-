23677e3e5458204156ab0a8257d2485b194380c1
0x1Luffy <chetankesare06@gmail.com>
2024-07-27 13:19:08 +0530

    Array of String: Done

--- Compound Datatypes/Arrays.rs
+++ Compound Datatypes/Arrays.rs
@@ -1,5 +1,10 @@
 fn main ()
 {
     let numbers: [i32; 5] = [1,2,3,4,5];
-    println!("Array is : {:?}", numbers ) // Debuggable format
+    println!("Array is : {:?}", numbers ); // Debuggable format
+
+    // Array of String
+
+    let fruits : [&str; 3] = ["Apple","Banana", "Orange"];
+    println!("Fruits are : {:?}", fruits);
 }
